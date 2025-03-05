bbb1c79867978d6150390e18deb088745eeabbeb
khdcd1234 <dhananjayakhdc@gmail.com>
2025-03-05 17:51:04 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,7 +2,7 @@ const settings = {
   packname: 'Knight',
   author: 'Bot',
   botName: "Knight Bot",
-  botOwner: 'Knight Bot', // Your name
+  botOwner: 'Knight-Bot{DEW}', // Your name
   ownerNumber: '94786936834', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
